352cb38d5d948d4a6a3f1c1bc09333c4af343a6e
peterkk2 <blissfulkaaya@gmail.com>
2024-09-22 12:35:58 +0300

    Add files via upload

---
+++ Do-While Loop/Do-While Loop.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+  int i = 1;
+  do {
+    std::cout << i << " ";
+    i++;
+  } while (i <= 5); // Looping while i is less than or equal to 5
+
+  std::cout << std::endl;
+  return 0;
+}
